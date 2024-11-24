65ce6d7b059e2791b1d9fd10d0cb7db01e9f7d36
Mau1361 <maudigraskar@gmail.com>
2024-11-24 20:53:00 +0530

    Initial commit of development branch structure

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+echo "package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}" > src/main/java/com/example/demo/DemoApplication.java
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,12 @@
+echo "package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(\"/\")
+    public String sayHello() {
+        return \"Hello, World!\";
+    }
+}" > src/main/java/com/example/demo/HelloController.java
