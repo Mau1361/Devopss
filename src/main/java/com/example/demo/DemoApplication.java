b8198010d78d54cc9557675265ae0286aadd148e
Mau1361 <108985823+Mau1361@users.noreply.github.com>
2024-11-25 00:05:52 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,9 @@ echo 'package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication;
+@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-}
+}' > src/main/java/com/example/demo/DemoApplication.java
