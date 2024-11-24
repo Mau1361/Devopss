5ff8bf29dfb266dab58adda3ffd14f1c2e8a116a
Mau1361 <108985823+Mau1361@users.noreply.github.com>
2024-11-24 23:59:56 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,12 @@
-echo "package com.example.demo;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping(\"/\")
+    @GetMapping("/")
     public String sayHello() {
-        return \"Hello, World!\";
+        return "Hello, World!";
     }
-}" > src/main/java/com/example/demo/HelloController.java
+}
